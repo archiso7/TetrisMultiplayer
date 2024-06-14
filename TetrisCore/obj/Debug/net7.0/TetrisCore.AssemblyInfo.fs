9d5475330d62c9bedca2f253ecc52dad613604eb
archiso <awesomelax7@gmail.com>
2024-06-13 21:28:38 -0400

    clean up libs

--- TetrisCore/obj/Debug/net7.0/TetrisCore.AssemblyInfo.fs
+++ TetrisCore/obj/Debug/net7.0/TetrisCore.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("TetrisCore")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c7749d70710ef8a8ee0a1c9a30c4a93d8913f4")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("TetrisCore")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("TetrisCore")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
